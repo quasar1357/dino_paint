a44f489401c9942cc64e9e8a1557bbb5a5839060
roman.schwob@students.unibe.ch <roman.schwob@students.unibe.ch>
2023-11-09 09:38:39 +0100

    Try making it a packege

---
+++ __init__.py.py
@@ -0,0 +1,1 @@
+import dino_paint_utils
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dino_paint',
+    version='0.1',
+    description='DINOv2 with VGG16 for semantic segmentation using a random forest classifier',
+    author='Roman Schwob',
+    author_email='roman.schwob@students.unibe.ch',
+    license='None',
+    packages=['package'],
+    scripts=[''],
+    zip_safe=False)
