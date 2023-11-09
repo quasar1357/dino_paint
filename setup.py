31282a09d4876e910969b3304feff2ed5ce812c0
roman.schwob@students.unibe.ch <roman.schwob@students.unibe.ch>
2023-11-09 10:27:40 +0100

    try using utils as package directly

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='dino-paint',
+setup(name='dinopaint',
     version='0.1',
     description='DINOv2 with VGG16 for semantic segmentation using a random forest classifier',
     author='Roman Schwob',
     author_email='roman.schwob@students.unibe.ch',
     license='GNU GPLv3',
-    packages=['package'],
+    packages=['dino_paint_utils.py'],
     zip_safe=False)
