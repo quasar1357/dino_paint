067c84b0289975bb6118710638cfa36134d347e4
roman.schwob@students.unibe.ch <roman.schwob@students.unibe.ch>
2023-11-09 10:21:25 +0100

    add install dino-paint

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='dino_paint',
+setup(name='dino-paint',
     version='0.1',
     description='DINOv2 with VGG16 for semantic segmentation using a random forest classifier',
     author='Roman Schwob',
